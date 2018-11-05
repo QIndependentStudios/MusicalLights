6254c401f408b2664db0c6a6df6606cab0a79f7e
Quoc-Thy Ngo <qngo.general@outlook.com>
2018-11-05 11:34:38 -0500

    Removed unnecessary methods for sequence model.

--- QIndependentStudios.MusicalLights.Core/Sequence.cs
+++ QIndependentStudios.MusicalLights.Core/Sequence.cs
@@ -10,9 +10,26 @@ namespace QIndependentStudios.MusicalLights.Core
         [JsonProperty(PropertyName = nameof(KeyFrames))]
         private List<KeyFrame> _keyFrames = new List<KeyFrame>();
 
-        public Sequence(int version = 1)
+        public Sequence(int version = 1) : this(new List<KeyFrame>(), version)
+        { }
+
+        public Sequence(IEnumerable<KeyFrame> keyFrames, int version = 1)
         {
+            if (keyFrames == null)
+                throw new ArgumentNullException(nameof(keyFrames));
+
             Version = version;
+
+            foreach (var keyFrame in keyFrames)
+            {
+                if (keyFrame == null)
+                    throw new ArgumentNullException(nameof(keyFrame));
+
+                if (_keyFrames.Any(f => f.Time == keyFrame.Time))
+                    throw new ArgumentException($"A KeyFrame already exists with time {keyFrame.Time}.", nameof(keyFrame));
+
+                _keyFrames.Add(keyFrame);
+            }
         }
 
         public int Version { get; set; }
@@ -29,53 +46,6 @@ namespace QIndependentStudios.MusicalLights.Core
         {
             _keyFrames = _keyFrames.OrderBy(f => f.Time).ToList();
             return JsonConvert.SerializeObject(this);
-        }
-
-        public void AddKeyFrame(KeyFrame keyFrame)
-        {
-            if (keyFrame == null)
-                throw new ArgumentNullException(nameof(keyFrame));
-
-            if (_keyFrames.Any(f => f.Time == keyFrame.Time))
-                throw new ArgumentException($"A KeyFrame already exists with time {keyFrame.Time}.", nameof(keyFrame));
-
-            _keyFrames.Add(keyFrame);
-        }
-
-        public void AddKeyFrames(IEnumerable<KeyFrame> keyFrames)
-        {
-            if (keyFrames == null)
-                throw new ArgumentNullException(nameof(keyFrames));
-
-            foreach (var keyFrame in keyFrames)
-            {
-                AddKeyFrame(keyFrame);
-            }
-        }
-
-        public void RemoveKeyFrame(KeyFrame keyFrame)
-        {
-            if (keyFrame == null)
-                throw new ArgumentNullException(nameof(keyFrame));
-
-            _keyFrames.Remove(keyFrame);
-        }
-
-        public void RemoveKeyFrame(TimeSpan time)
-        {
-            if (time == null)
-                throw new ArgumentNullException(nameof(time));
-
-            var match = _keyFrames.FirstOrDefault(f => f.Time == time);
-            if (match == null)
-                return;
-
-            _keyFrames.Remove(match);
-        }
-
-        public void ClearKeyFrames()
-        {
-            _keyFrames.Clear();
         }
     }
 }
